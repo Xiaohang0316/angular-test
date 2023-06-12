35d00fb4d17080cd062d9a8e39206d1b2b41e153
Xiaohang0316 <84364287+Xiaohang0316@users.noreply.github.com>
2023-06-12 21:39:54 +0800

    moment

--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
 
 @Component({
-  selector: 'app-test3',
-  templateUrl: './test3.component.html',
-  styleUrls: ['./test3.component.less']
+    selector: 'app-test3',
+    templateUrl: './test3.component.html',
+    styleUrls: ['./test3.component.less']
 })
 export class Test3Component implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    constructor() { }
+    moment = moment;
+    ngOnInit(): void {
+        // const aa = moment('2012/12/12').format();
+        // console.log(aa)
+    }
 
 }
